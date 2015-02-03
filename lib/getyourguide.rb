8dfe545bc1b1a380fca0d47741adf5b1a5585647
Rémi Delhaye <remi.delhaye@epsi.fr>
2015-02-03 18:48:44 +0100

    Add Class generation

--- lib/getyourguide.rb
+++ lib/getyourguide.rb
@@ -1,19 +1,36 @@
-module GetYourGuide
-end
-
 require "getyourguide/version"
+require "getyourguide/model_attributes"
 require "getyourguide/api_client"
 require "getyourguide/errors/api_error"
-require "getyourguide/models/activity"
-require "getyourguide/models/category"
-require "getyourguide/models/destination"
-require "getyourguide/models/image"
-require "getyourguide/models/image_format"
-require "getyourguide/models/location"
-require "getyourguide/models/top_category"
 require "getyourguide/parsers/default"
 require "getyourguide/parsers/categories"
 require "getyourguide/parsers/destinations"
 require "getyourguide/parsers/image_formats"
 require "getyourguide/parsers/products"
 require "getyourguide/parsers/top_categories"
+
+module GetYourGuide
+  module Models
+    GetYourGuide::MODEL_NAMES.each do |klass_name|
+      klass_vars  = GetYourGuide::MODEL_ATTRIBUTES[klass_name.to_s]
+      klass       = Class.new do
+        klass_vars.each do |field|
+          define_method field.intern do
+            instance_variable_get("@#{field}")
+          end
+          define_method "#{field}=".intern do |arg|
+            instance_variable_set("@#{field}", arg)
+          end
+        end
+        define_method :initialize do |args|
+          klass_vars.each do |field|
+            binding.pry
+            instance_variable_set("@#{field}", args[field])
+          end
+        end
+      end
+
+      const_set klass_name, klass
+    end
+  end
+end
